5e6549a23c9f906faf51d38353e0cc28247a518b
FelipeOtz <felipeht25@gmail.com>
2022-09-14 15:47:17 -0300

    teste de consumo API finalizado

--- App.jsx
+++ App.jsx
@@ -5,9 +5,11 @@ import { useEffect } from "react";
 
 export default function App() {
   useEffect(() => {
-    fetch("http://localhost:8080/produtos/find/19")
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+    fetch("http://192.168.56.1:8080/produtos/find/19").then((response) => {
+      response.json().then((data) => {
+        console.log(data);
+      });
+    });
   });
 
   return (
